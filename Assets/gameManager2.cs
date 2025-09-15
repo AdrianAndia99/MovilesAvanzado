2ae766ae53a833ec11ea27f9d5ce144f09854903
Adrian Andia <151102699+AdrianAndia99@users.noreply.github.com>
2025-09-15 15:54:15 -0500

    ga

---
+++ Assets/gameManager2.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using Unity.Netcode;
+using System.Collections.Generic;
+using System;
+
+public class gameManager2 : NetworkBehaviour
+{
+    public static gameManager2 Instance;
+    public Dictionary<string, PlayerData> playerStatesByAccountID = new();
+
+    public GameObject playerPrefab;
+    public Action OnConnection;
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+            NetworkManager.Singleton.OnClientDisconnectCallback += HandleDisconnect;
+            OnConnection?.Invoke();
+    }
+    public override void OnNetworkDespawn()
+    {
+        if (IsServer)
+        {
+            NetworkManager.Singleton.OnClientDisconnectCallback += HandleDisconnect;
+        }
+    }
+    private void HandleDisconnect(ulong clientID)
+    {
+        print("jugador " + clientID + " se fue");
+
+    }
+
+    [Rpc(SendTo.Server)]
+    public void RegisterPlayerServerRpc(string accountID, ulong ID)
+    {
+        if (!playerStatesByAccountID.TryGetValue(accountID, out PlayerData data))
+        {
+            PlayerData newData = new PlayerData(accountID, Vector3.zero, 100, 5);
+            playerStatesByAccountID[accountID] = newData;
+            // instanciar player con data
+            SpawnPlayerServer(ID, newData);
+            print("nuevo jugador " + accountID);
+        }
+        else
+        {
+            print("bienvenido de nuevo " + accountID);
+            SpawnPlayerServer(ID, data);
+
+        }
+    }
+    public void SpawnPlayerServer(ulong ID, PlayerData data)
+    {
+        if (!IsServer) return;
+        GameObject player = Instantiate(playerPrefab);
+        player.GetComponent<NetworkObject>().SpawnAsPlayerObject(ID, true);
+        player.GetComponent<player2>().SetData(data);        
+    }
+}
---
+++ Assets/uiGameManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+
+public class uiGameManager : NetworkBehaviour
+{
+    public TMP_InputField inputField;
+    public Button ConnectButton;
+    public GameObject loginPanel;
+
+    void Start()
+    {
+        ConnectButton.onClick.AddListener(OnSubmitName);
+        loginPanel.SetActive(false);
+
+        gameManager2.Instance.OnConnection += () =>
+        {
+            loginPanel.SetActive(true);
+            inputField.text = "";
+            ConnectButton.interactable = true;
+            inputField.interactable = true;
+        };
+    }
+    public void OnSubmitName()
+    {
+        string accountID = inputField.text;
+        if (!string.IsNullOrEmpty(accountID))
+        {
+            gameManager2.Instance.RegisterPlayerServerRpc(accountID, NetworkManager.Singleton.LocalClientId);
+            ConnectButton.interactable = false;
+            inputField.interactable = false;
+
+            loginPanel.SetActive(false);
+        }
+    }
+}
